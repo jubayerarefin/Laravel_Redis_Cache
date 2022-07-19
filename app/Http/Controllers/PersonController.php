2ad42ce2ce1aafa1d436d975e84eb54f397d60a0
Sharif Jubayer Arefin <18138076+jubayerarefin@users.noreply.github.com>
2022-07-19 16:35:32 +0600

    Adds a macro to enable LengthAwarePaginator with collection object into AppServiceProvider
    Adds pagination in index.blade
    
    Paginates filtered data from cache using a collection

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StorePersonRequest;
-use App\Http\Requests\UpdatePersonRequest;
-use App\Models\Person;
-use GuzzleHttp\Psr7\Request;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
@@ -18,10 +14,10 @@ class PersonController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @return Application|Factory|View
      */
-    public function index(\Illuminate\Http\Request $request): View|Factory|Application
+    public function index(Request $request): View|Factory|Application
     {
         $validatedData = $request->validate([
             'month' => ['numeric', 'max:12', 'min:1'],
@@ -45,6 +41,8 @@ class PersonController extends Controller
 
             Cache::tags([$validatedData['month'], $validatedData['year']])->put('people', $people, 60);
         }
+
+        $people = collect($people)->paginate(20)->withQueryString();;
 
         return view('people.index', compact('people'));
     }
--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -1,10 +1,13 @@
 <html lang="">
 <body>
 <h1>People</h1>
-<ul>
-    @foreach ($people as $person)
-        <li>{{ $person->name}} {{$person->dob}}</li>
-    @endforeach
-</ul>
+<div class="container">
+    <ul>
+        @foreach ($people as $person)
+            <li>{{ $person->name}} {{$person->dob}}</li>
+        @endforeach
+    </ul>
+</div>
+{{ $people->links() }}
 </body>
 </html>
