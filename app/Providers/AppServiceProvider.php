7fdb3e2538f386dcc098a0c879ba8fab470e415a
Sharif Jubayer Arefin <18138076+jubayerarefin@users.noreply.github.com>
2022-07-23 09:20:36 +0600

    Removes unnecessary codes from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,19 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Collection::macro('paginate', function ($perPage, $total = null, $page = null, $pageName = 'page') {
-            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
 
-            return new LengthAwarePaginator(
-                $total ? $this : $this->forPage($page, $perPage)->values(),
-                $total ?: $this->count(),
-                $perPage,
-                $page,
-                [
-                    'path' => LengthAwarePaginator::resolveCurrentPath(),
-                    'pageName' => $pageName,
-                ]
-            );
-        });
     }
 }
